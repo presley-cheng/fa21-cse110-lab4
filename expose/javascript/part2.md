9b53dfa68fc93a41d3209ca4b05c55ebd6c716cc
presley-cheng <presley6078@gmail.com>
2021-10-17 02:02:58 -0700

    finish 3 questions for part 2, create a js file for testing

---
+++ expose/javascript/part2.md
@@ -0,0 +1,8 @@
+** Part 2
+* Question 1
+print the number 3, because i was declared through a var keyword in the for loop, which persists beyond blocks, and since i was incremented on every iteration of the loop plus the last time it was incremented to exit out of the loop, we get 3.
+* Question 2
+print the number 150, because discountedPrice was declared using a var keyword, which means it is visible within the entire 'discountPrices' function, or in other words, persists after the block it was created.
+* Question 3
+print the number 150, finalPrice was also declared using the var keyword at the beginning of the function, therefore, it is visible throughout the entire 'discountPrices' function.
+* Question 4
--- expose/javascript/test.js
+++ expose/javascript/test.js
@@ -1,11 +1,17 @@
-function sumValues(num1, num2, add) {
-    if (add) {
-        const result = 0;
-        result = num1 + num2;
-        console.log('values added: ', result);
-    } else return;
+function test(prices, discount) {
+       var discounted = [];
+       var finalPrice = 0;
 
-    console.log('final result: ', result);
+       for (var i = 0; i < prices.length; i++) {
+               var discountedPrice = prices[i] * (1 - discount);
+               finalPrice = Math.round(discountedPrice * 100) / 100;
+               discounted.push(finalPrice);
+       }
+
+       // console.log(i);
+       // console.log(discountedPrice);
+       console.log(finalPrice);
+       return discounted;
 }
 
-sumValues(10, 10, true);
+test([100, 200, 300], 0.5);
